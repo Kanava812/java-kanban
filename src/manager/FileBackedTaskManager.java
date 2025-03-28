72eba6e711d3e5c3701ca95cd1568462a5fcbb60
Kanava <knvznr@yandex.ru>
2025-03-28 07:18:15 +0300

    фз 8-го спринта

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -19,24 +19,22 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     protected void saveToFile() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(TaskCsvFormatHandler.getHeader() + "\n");
-            String tasksString= tasks.values().stream()
-                    .map(task -> TaskCsvFormatHandler.toString(task)  + "\n")
+            String tasksString = tasks.values().stream()
+                    .map(task -> TaskCsvFormatHandler.toString(task) + "\n")
                     .collect(Collectors.joining());
-
-            String epicsString= epics.values().stream()
+            String epicsString = epics.values().stream()
                     .filter(epic -> epic.getStartTime() != null)
-                    .map(epic -> TaskCsvFormatHandler.toString(epic)  + "\n")
+                    .map(epic -> TaskCsvFormatHandler.toString(epic) + "\n")
                     .collect(Collectors.joining());
-
-            String subtasksString= subtasks.values().stream()
-                    .map(subtask -> TaskCsvFormatHandler.toString(subtask)  + "\n")
+            String subtasksString = subtasks.values().stream()
+                    .map(subtask -> TaskCsvFormatHandler.toString(subtask) + "\n")
                     .collect(Collectors.joining());
-
-            writer.write(tasksString+epicsString+subtasksString);
+            writer.write(tasksString + epicsString + subtasksString);
         } catch (IOException e) {
             throw new ManagerSaveException("Ошибка сохранения задач в файл.", e);
         }
     }
+
 
     public static FileBackedTaskManager loadFromFile(File file) {
         final FileBackedTaskManager manager = new FileBackedTaskManager(file);
@@ -47,9 +45,9 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
                 String line = lines[i];
                 Task task = TaskCsvFormatHandler.fromString(line);
                 switch (task.getType()) {
-                   case Type.TASK -> manager.createTask(task);
-                   case Type.EPIC -> manager.createEpic((Epic) task);
-                   case Type.SUBTASK -> manager.createSubtask((Subtask) task);
+                    case Type.TASK -> manager.createTask(task);
+                    case Type.EPIC -> manager.createEpic((Epic) task);
+                    case Type.SUBTASK -> manager.createSubtask((Subtask) task);
                 }
                 if (loadedId < task.getId()) {
                     loadedId = task.getId();
@@ -62,6 +60,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return manager;
     }
 
+
     @Override
     public Task createTask(Task task) {
         Task t = super.createTask(task);
@@ -70,12 +69,14 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return t;
     }
 
+
     @Override
     public Epic createEpic(Epic epic) {
         Epic e = super.createEpic(epic);
         saveToFile();
         return e;
     }
+
 
     @Override
     public Subtask createSubtask(Subtask subtask) {
@@ -84,6 +85,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return s;
     }
 
+
     @Override
     public Task updateTask(Task task) {
         Task t = super.updateTask(task);
@@ -91,11 +93,13 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return t;
     }
 
+
     @Override
     public void updateEpic(Epic epic) {
         super.updateEpic(epic);
         saveToFile();
     }
+
 
     @Override
     public Subtask updateSubtask(Subtask subtask) {
@@ -104,11 +108,13 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return s;
     }
 
+
     @Override
     public void deleteTask(int id) {
         super.deleteTask(id);
         saveToFile();
     }
+
 
     @Override
     public void deleteEpic(int id) {
@@ -116,16 +122,19 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         saveToFile();
     }
 
+
     @Override
     public void deleteSubtask(int id) {
         super.deleteSubtask(id);
         saveToFile();
     }
 
+
     @Override
     public List<Task> getHistory() {
         return super.getHistory();
     }
+
 
     @Override
     public void deleteAllTasks() {
